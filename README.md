6c8ffc0ff4a11a213e975a8e1f50931084d32d4f
boxman42 <noahmurrant@gmail.com>
2024-01-30 14:00:41 -0500

    added views.py and initial app testing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSCI485_software_project
-this repository contains all the scripts for the CSCI485 software project
+this repository contains all the scripts for the stfx CSCI485 software project.
+Flask is used for backend.
--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 """
 This is the main script for the project. it contains the nessary functions for the website to run
 """
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "this is the home page"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
